0ae1671651d1467ebedeb81b18505cc56fabf177
asadulla2007 <110012942+asadulla2007@users.noreply.github.com>
2022-07-30 03:17:57 -0400

    Add files via upload

---
+++ son 5 ga bolinadimi    if/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 10; // berilgan son
+
+        // qoldiq 0ga teng bo'lsa demak 5ga bo'linadi.
+
+        if (a % 5 == 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
